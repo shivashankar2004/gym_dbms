14267f250570616442eb32f724b5f87b2821d1b1
Saran-067 <saranrajasekaran067@email.com>
2024-05-12 07:32:59 +0530

    video 5

---
+++ get_plan.php
@@ -0,0 +1,12 @@
+<?php
+include("db.php"); I
+$stmt = $conn->prepare("select pid, planname, validity, amount from plan order by pid DESC");
+$stmt->bind_result($pid, $planname, $validity, $amount);
+if ($stmt->execute()) {
+    while ( $stmt->fetch() ) {
+        $output[] = array ("pid"=>$pid, "planname" => $planname, "validity" => $validity, "amount" => $amount);
+    }
+    echo json_encode( $output );
+}
+$stmt->close();
+//}
