e7e09c5a03d8a988f1edf6a34df40609a9cd2c1f
Saran-067 <saranrajasekaran067@email.com>
2024-05-12 08:11:05 +0530

    video 6

---
+++ header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-inverse">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href="#"><b>Focus Fitness</b></a>
+</div>
+<ul class="nav navbar-nav">
+<li><a href="index.php">Home</a></li>
+<li><a href="registation.php">Registation</a></li>
+<li><a href="payment.php">Payment</a></li>
+<li><a href="plan.php" >Plan</a></li>
+I
+<li><a href="report.php">Report</a></li>
+<li><a href="member.php">View Members</a></li>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+</ul>
+</div>
+</nav>
